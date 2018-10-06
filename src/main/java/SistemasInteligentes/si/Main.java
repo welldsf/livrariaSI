8b78cd668c50accae70a0bb80641a1a6042347df
Lucas Mauro <lucasmaurodesouza@gmail.com>
2018-10-06 17:18:57 -0300

    Capturar alugueis por usuarios

--- src/main/java/SistemasInteligentes/si/Main.java
+++ src/main/java/SistemasInteligentes/si/Main.java
@@ -1,19 +1,29 @@
 package SistemasInteligentes.si;
 
 import org.semanticweb.owlapi.model.OWLNamedIndividual;
+import org.semanticweb.owlapi.model.OWLObjectPropertyAssertionAxiom;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
-import org.semanticweb.owlapi.reasoner.NodeSet;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
 
     private static final String ONTOLOGY_PATH = "src/main/java/resources/livraria.owl";
     private static final String USUARIO = "Usuario";
-    private static final String LIVRO = "Livro";
+    private static final String ALUGOU = "alugou";
 
     public static void main(String args[]) throws OWLOntologyCreationException {
         OntologyHelper helper = new OntologyHelper(ONTOLOGY_PATH);
 
-        NodeSet<OWLNamedIndividual> usuarios = helper.getIndividualsOf(USUARIO);
-        NodeSet<OWLNamedIndividual> livros = helper.getIndividualsOf(LIVRO);
+        List<OWLNamedIndividual> usuarios = helper.getIndividualsOf(USUARIO);
+        Map<OWLNamedIndividual, List<OWLObjectPropertyAssertionAxiom>> alugueisPorUsuario = new HashMap<>();
+
+        usuarios.forEach(it -> {
+            String usuario = it.getIRI().getShortForm();
+            List<OWLObjectPropertyAssertionAxiom> axioms = helper.getAxioms(usuario, ALUGOU);
+            alugueisPorUsuario.put(it, axioms);
+        });
     }
 }
--- src/main/java/SistemasInteligentes/si/OntologyHelper.java
+++ src/main/java/SistemasInteligentes/si/OntologyHelper.java
@@ -2,12 +2,13 @@ package SistemasInteligentes.si;
 
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.*;
-import org.semanticweb.owlapi.reasoner.NodeSet;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
 import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
 import org.semanticweb.owlapi.reasoner.structural.StructuralReasonerFactory;
 
 import java.io.File;
+import java.util.List;
+import java.util.stream.Collectors;
 
 class OntologyHelper {
 
@@ -22,11 +23,21 @@ class OntologyHelper {
         reasoner = reasonerFactory.createReasoner(ontology);
     }
 
-    NodeSet<OWLNamedIndividual> getIndividualsOf(String className) {
+    List<OWLNamedIndividual> getIndividualsOf(String className) {
         OWLClass owlClass = ontology.classesInSignature()
                 .filter(it -> it.getIRI().getShortForm().equals(className))
                 .findFirst()
                 .get();
-        return reasoner.getInstances(owlClass, false);
+
+        return reasoner.getInstances(owlClass, false)
+                .entities()
+                .collect(Collectors.toList());
+    }
+
+    List<OWLObjectPropertyAssertionAxiom> getAxioms(String subject, String property) {
+        return ontology.axioms(AxiomType.OBJECT_PROPERTY_ASSERTION)
+                .filter(it -> it.getSubject().toString().contains(subject) &&
+                        it.getProperty().getNamedProperty().getIRI().getShortForm().equals(property))
+                .collect(Collectors.toList());
     }
 }
